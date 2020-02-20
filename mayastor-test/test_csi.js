dfab1f5f604c97d94f815bcc8730354f5722492a
blaisedias <blaised02@gmail.com>
2020-02-20 16:46:41 +0000

    Stop CSI tests from aborting: add share field to publish_nexus call.

--- mayastor-test/test_csi.js
+++ mayastor-test/test_csi.js
@@ -197,7 +197,7 @@ describe('csi', function() {
               let uuid = BASE_UUID + n;
               common.dumbCommand(
                 'publish_nexus',
-                { uuid: uuid, key: '' },
+                { uuid: uuid, key: '', share: 0 },
                 next
               );
             },
