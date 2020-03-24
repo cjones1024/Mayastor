94d78ef5c45d51693a4f85c8f134259bf02ca7c3
blaisedias <blaise.dias@mayastor.io>
2020-03-24 08:49:03 +0000

    Address Code-review comments:
    Revert deploy/pvc.yaml to use 64Mi, we can discuss later
    what it should really be, that is not a requirement of this change.
    Remove TODO, JIRA already exists for the TODO.

--- mayastor-test/test_csi.js
+++ mayastor-test/test_csi.js
@@ -201,7 +201,6 @@ describe('csi', function() {
                 {
                   uuid: uuid,
                   key: '',
-                  // TODO: repeat this test for iSCSI and Nvmf
                   share: mayastorProto.getConstants().ShareProtocolNexus
                     .NEXUS_NBD,
                 },
